954e306a3cd21f50332f61170b27b4f33833bfe6
SNDev <34571022+Nkoinathi-Sola@users.noreply.github.com>
2020-02-18 23:11:51 +0200

    changed button styles

--- src/main/java/com/nkosinathi/app/App.java
+++ src/main/java/com/nkosinathi/app/App.java
@@ -9,24 +9,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Hello world!
- *
- */
-public class App {
-        public static void main(String[] args) {
 
+public class App {
+
+    public static void main(String[] args) {
             final JFrame frame = new JFrame();
             JPanel panel = new JPanel();
 
-            JButton button1 = new JButton("Click me to access my link.", new ImageIcon("C:\\Users\\nkosi\\IdeaProjects\\CloningAssignment\\src\\animate.gif"));
-            //JButton button1 = new JButton();
+            JButton button1 = new JButton("Click me to access my link.", new ImageIcon("C:\\Users\\nkosi\\IdeaProjects\\FirstMavenProject\\src\\main\\java\\animate.gif"));
             button1.setToolTipText("Click me to access my link.");
             button1.setPreferredSize(new Dimension(200, 35));
 
 
             button1.setForeground(Color.BLACK);
-            button1.setBackground(Color.BLUE);
+            button1.setBackground(Color.YELLOW);
             Border line = new LineBorder(Color.WHITE);
             Border margin = new EmptyBorder(5, 15, 5, 15);
             Border compound = new CompoundBorder(line, margin);
@@ -45,15 +41,17 @@ public class App {
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
 
-            button1.addActionListener(new ActionListener() {
 
-                public void actionPerformed(ActionEvent arg0) {
-                    JOptionPane.showMessageDialog(frame.getComponent(0), "Hi there I'm a git repo from https://github.com/Nkoinathi-Sola/FirstMavenProject.git ... Clone me and modify me for better features.");
-                    label.setVisible(true);
-                }
-            });
+                button1.addActionListener(new ActionListener() {
+
+                    public void actionPerformed(ActionEvent arg0) {
+                        JOptionPane.showMessageDialog(frame.getComponent(0), "Hi there I'm a git repo from https://github.com/Nkoinathi-Sola/FirstMavenProject.git ... Clone me and modify me for better features.");
+                        label.setVisible(true);
+                    }
+                });
+
+            }
 
         }
 
-    }
 
